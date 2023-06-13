698705f68cdcbafab80c8bfc0dc51939271d6e3b
Muharrem Selman YILMAZ <mselman.yilmaz@spk.gov.tr>
2023-06-13 10:27:51 +0300

    Add project files.

---
+++ DevFramework.DataAccess.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevFramework.DataAccess.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SPK")]
+[assembly: AssemblyProduct("DevFramework.DataAccess.Tests")]
+[assembly: AssemblyCopyright("Copyright © SPK 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b374cfc3-25c1-4b48-9f43-681f7ea5c852")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DevFramework.NorthwindBusiness.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevFramework.NorthwindBusiness.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SPK")]
+[assembly: AssemblyProduct("DevFramework.NorthwindBusiness.Tests")]
+[assembly: AssemblyCopyright("Copyright © SPK 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b0787996-eca0-4a78-9f79-fd2815952bc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
