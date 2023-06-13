1096dbb64aab771cbd13994c495d92445db327c1
Muharrem Selman YILMAZ <mselman.yilmaz@spk.gov.tr>
2023-06-13 12:05:58 +0300

    EfEntityRepositoryBase and IQueryableRepository added the solution.

---
+++ DevFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,66 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public TEntity Add(TEntity entity)
+        {
+            using(var context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using(var context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter); //return the data that matches the filter
+            }
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using(var context = new TContext())
+            {
+                return filter == null 
+                    ? context.Set<TEntity>().ToList() //if filter is null return all context..
+                    : context.Set<TEntity>().Where(filter).ToList(); //if filter is set return the context that matches the filter..
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using(var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+    }
+}
